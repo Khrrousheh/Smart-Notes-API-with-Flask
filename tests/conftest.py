e0d62859859f6022a5471cf6555b5721d4f037a4
Mahdi Khrrousheh <mahdi.khrrousheh@gmail.com>
2025-07-14 18:09:24 +0300

    flake: fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,7 @@
 # tests/conftest.py
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import pytest
 from app import create_app, db
+
 
 @pytest.fixture
 def app():
@@ -19,6 +16,7 @@ def app():
         yield app
         db.session.remove()
         db.drop_all()
+
 
 @pytest.fixture
 def client(app):
