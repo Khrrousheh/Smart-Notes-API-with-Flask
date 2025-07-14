36c529dc0ccf3cd082a0ad58b93c46dd75848b60
Mahdi Khrrousheh <mahdi.khrrousheh@gmail.com>
2025-07-14 22:42:17 +0300

    database: use postgresql

--- app/config.py
+++ app/config.py
@@ -3,8 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///notes.db")
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
