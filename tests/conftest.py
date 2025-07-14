4b1e317bc10004eb8c4fb85849004c28fdeeff0f
Mahdi Khrrousheh <mahdi.khrrousheh@gmail.com>
2025-07-14 18:11:33 +0300

    flake: fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 # tests/conftest.py
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
+
 from app import create_app, db
 
 
